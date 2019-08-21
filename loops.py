c4afdc267ec95cc3f0bd60aa6c202cf57ec86032
itisnarendra <npershadca@yahoo.ca>
2019-08-21 12:25:12 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -16,3 +16,12 @@ for i, color in enumerate(colors):
 numbers = [1, 2, 3, 4, 5]
 for number, color in zip(numbers, colors):
   print(f'{number} --> {color}')
+
+services = {'ftp': 21, 'ssh': 22, 'smtp': 25, 'http': 80, 'pop3': 110, 'imap': 220, 'https': 443}
+#for loop
+for key in services:
+	print(key)
+
+  
+for key, value in services.items():
+  print(key, value)
