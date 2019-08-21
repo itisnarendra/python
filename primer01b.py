ce62e0689e01d5e2fa1baaec490fb5b7fe94d9ba
itisnarendra <npershadca@yahoo.ca>
2019-08-21 10:30:51 -0400

    Update primer01b.py

--- primer01b.py
+++ primer01b.py
@@ -1,11 +1,33 @@
 def hello():
-	'''prints hello world'''
+	'''This function does not take any arguments
+	and it does not return any value.
+	It prints Hello World'''
 	print("Hello World!")
 
-def hello_world(name):
-	'''prints hello world'''
-	print('Hello World from', name)
+help(hello)
 
-def sum(a, b):
-	'''a function to add two numbers'''
+
+def hello_world(name):
+	'''This function take a single string argument
+	and it does not return any value.
+	It prints Hello World from {argument}'''
+	print(f'Hello World from {name}')
+
+
+def add(a, b):
+	'''This function take two argument and
+	returns the sum of them'''
 	return a + b
+
+sum = add(10, 5)
+sum			#displays 15
+
+
+def add_minus(a, b):
+	'''This function take two argument and
+	returns the sum and difference of them'''
+	return a + b, a - b
+
+sum, diff = add_minus(5, 2)
+sum			#display2 7
+diff			#displays 2
