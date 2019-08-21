209d2d5e2e912e8ff867c9eb51e180d8e642e1b3
itisnarendra <npershadca@yahoo.ca>
2019-08-21 11:01:05 -0400

    Update import.py

--- import.py
+++ import.py
@@ -21,8 +21,11 @@ from math import *
 >>> dir()
         # ['__annotations__', '__builtins__', '__doc__', '__loader__', '__name__', '__package__', '__spec__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'ceil', 'copysign', 'cos', 'cosh', 'degrees', 'e', 'erf', 'erfc', 'exp', 'expm1', 'fabs', 'factorial', 'floor', 'fmod', 'frexp', 'fsum', 'gamma', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'ldexp', 'lgamma', 'log', 'log10', 'log1p', 'log2', 'modf', 'nan', 'np', 'pi', 'pow', 'radians', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'tau', 'trunc']
 
-sin(0.5326)
+sin(0.5326)     #angle is in radians
 0.507774928359006
+
+sin(radians(30))
+0.49999999999999994
 
 import numpy as np      #import the numpy and assigns it to the alias np
 
