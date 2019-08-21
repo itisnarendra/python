c88fc6483a2f37468e46e695b50e431505f8cb68
itisnarendra <npershadca@yahoo.ca>
2019-08-21 10:14:15 -0400

    Create primer01b.py

---
+++ primer01b.py
@@ -0,0 +1,11 @@
+def hello():
+	'''prints hello world'''
+	print("Hello World!")
+
+def hello_world(name):
+	'''prints hello world'''
+	print('Hello World from', name)
+
+def sum(a, b):
+	'''a function to add two numbers'''
+	return a + b
