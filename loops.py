2d9e2dcb0115cf75b87bc384dc51e3cda70499e8
itisnarendra <npershadca@yahoo.ca>
2019-08-21 12:19:18 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -4,6 +4,15 @@ for color in colors:
   print(color)
   
 
-for color in reverse(color):
+for color in reversed(colors):
+  print(color)
+  
+for color in sorted(colors):
   print(color)
  
+for i, color in enumerate(colors):
+  print(f'{i} --> {color}')
+  
+numbers = [1, 2, 3, 4, 5]
+for number, color in zip(numbers, colors):
+  print(f'{number} --> {color}')
